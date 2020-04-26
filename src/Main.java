9b6c3c6099212386c4d0f6babef2eac51d76dcb4
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-26 08:31:46 +0800

    IsiAir dengan While

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String [] args) {
-        int isiGelas = 225;
+        int isiGelas = 0;
         int penuh = 225;
 
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
 
-        do{
+        while(isiGelas != penuh){
             isiGelas++;
             System.out.println("Sedang mengisi Gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }while(isiGelas <= penuh);
+        }
         System.out.println("Finale isi Gelas Sekarang : " + isiGelas + "ml");
     }
 }
