beded60817438ae4494390e86b4dd4f2b2062b16
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-26 08:31:19 +0800

    IsiAir dengan DoWhile

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String [] args) {
-        int isiGelas = 0;
+        int isiGelas = 225;
         int penuh = 225;
 
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
 
-        for(isiGelas = 0; isiGelas <= penuh ; isiGelas++){
+        do{
             isiGelas++;
             System.out.println("Sedang mengisi Gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
-        }
+        }while(isiGelas <= penuh);
         System.out.println("Finale isi Gelas Sekarang : " + isiGelas + "ml");
     }
 }
