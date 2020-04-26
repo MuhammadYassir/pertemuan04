a1ccae52731c66583a26173ee7219dd3f5e9458b
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-26 09:43:41 +0800

    Delete Array.java

--- src/Array.java
+++
@@ -1,8 +0,0 @@
-public class Array {
-    public static void main(String [] args){
-        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
-        System.out.println(nilaiArr.length);
-        System.out.println(nilaiArr[7]);
-        System.out.println(nilaiArr[3]);
-    }
-}
