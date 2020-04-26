403295370230bdf74be9d7eed3c145bf7762497a
MuhammadYassir <62455195+MuhammadYassir@users.noreply.github.com>
2020-04-26 09:52:26 +0800

    Delete Array.java

--- src/Array.java
+++
@@ -1,7 +0,0 @@
-public class Array {
-    public static void main(String [] args) {
-        String na[] = {"Muhammad", "ZIDA", "Yassir"};
-        System.out.println(na[0]);
-        System.out.println(na[2]);
-    }
-}
