5d4c9a7455ab761d8076d42ae14768fc728f4a08
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-26 09:52:23 +0800

    program nama Array

---
+++ src/array.java
@@ -0,0 +1,7 @@
+public class array {
+        public static void main(String [] args) {
+            String na[] = {"Muhammad", "ZIDA", "Yassir"};
+            System.out.println(na[0]);
+            System.out.println(na[2]);
+        }
+    }
