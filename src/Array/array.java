92b403b6b96b052a6fe18b7697ec7c0fa9a76b9d
M.ZidaYassir <m.zidayassir122@gmail.com>
2020-04-27 10:46:56 +0800

    program Array

---
+++ src/Array/array.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class array {
+        public static void main(String [] args) {
+            String na[] = {"Muhammad", "ZIDA", "Yassir"};
+            System.out.println(na[0]);
+            System.out.println(na[2]);
+        }
+    }
